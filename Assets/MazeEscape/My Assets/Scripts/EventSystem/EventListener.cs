40ad4f899b4d21fbeef746546a10729abc244afb
Petr Šulc <95595719+Petross808@users.noreply.github.com>
2024-11-19 16:03:17 +0000

    Updated GameEvent System
    - Added DebugLog option
    - CallbackContext is now a struct
    - EventListener validation now clears the console

--- Assets/MazeEscape/My Assets/Scripts/EventSystem/EventListener.cs
+++ Assets/MazeEscape/My Assets/Scripts/EventSystem/EventListener.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Reflection;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.Events;
 
@@ -15,6 +16,22 @@ public class EventListener : MonoBehaviour
 
     public void OnValidate()
     {
+#if UNITY_EDITOR
+        ClearConsole();
+
+        static void ClearConsole()
+        {
+            var assembly = Assembly.GetAssembly(typeof(SceneView));
+            var type = assembly.GetType("UnityEditor.LogEntries");
+            var method = type.GetMethod("Clear");
+            method.Invoke(new object(), null);
+        }
+#endif
+        ValidateSignature();
+    }
+
+    public void ValidateSignature()
+    {
         if (_listeningTo == null)
             throw new System.NullReferenceException($"No event set in EventListener '{this.name}'.");
 
@@ -22,10 +39,10 @@ public class EventListener : MonoBehaviour
         {
             string methodName = _response.GetPersistentMethodName(i);
             UnityEngine.Object target = _response.GetPersistentTarget(i);
-            EventSignatureAttribute attribute = target?.GetType()?.GetMethod(methodName).GetCustomAttribute<EventSignatureAttribute>();
+            EventSignatureAttribute attribute = target?.GetType()?.GetMethod(methodName)?.GetCustomAttribute<EventSignatureAttribute>();
             if (attribute == null)
                 throw new System.Exception(
-                    $"Cannot register GameEvent to EventListener on GameObject '{this.name}'\n" +
+                    $"Cannot register EventListener on GameObject '{this.name}' to GameEvent\n" +
                     $"EventSignature attribute not found on response at index {i} ('{target}.{methodName}').");
 
             System.Type[] responseSignature = attribute.Signature;
@@ -34,7 +51,7 @@ public class EventListener : MonoBehaviour
                 eventSignature.Add(e.Type);
 
             Exception exception = new(
-                $"Cannot register GameEvent to EventListener on GameObject '{ this.name }'\n" +
+                $"Cannot register GameEvent to EventListener on GameObject '{this.name}'\n" +
                 $"GameEvent does not match signature of response at index {i}\n" +
                 $"{_listeningTo.name}: [{string.Join(",", eventSignature)}]\n" +
                 $"{methodName}: [{string.Join<Type>(",", responseSignature)}]"
@@ -43,7 +60,7 @@ public class EventListener : MonoBehaviour
             if (responseSignature.Length != eventSignature.Count)
                 throw exception;
 
-            for(int j = 0; j < responseSignature.Length; j++)
+            for (int j = 0; j < responseSignature.Length; j++)
             {
                 if (responseSignature[j] != eventSignature[j])
                     throw exception;
