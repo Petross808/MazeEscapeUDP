69ff4048de63d5110bd8e45bf146355642ac7ed5
Petr Šulc <95595719+Petross808@users.noreply.github.com>
2024-11-21 03:37:56 +0000

    Updating Player

--- Assets/MazeEscape/My Assets/Scripts/Player/CameraScript.cs
+++ Assets/MazeEscape/My Assets/Scripts/Player/CameraScript.cs
@@ -5,6 +5,7 @@ public class CameraScript : MonoBehaviour
 {
     [SerializeField] private Transform _camera;
     [SerializeField] private Vector2 _cameraSensitivity;
+    [SerializeField, Range(10, 90)] private int _maxVerticalAngle = 80;
 
     public void AimChanged(Vector2 delta)
     {
@@ -12,7 +13,7 @@ public class CameraScript : MonoBehaviour
         Vector3 newRotation = Vector3.zero;
 
         //Convert from (90 to 0/360 to 270) range to (90 to 0 to -90) range, add the delta and clamp to (-80, 80)
-        newRotation.x = Mathf.Clamp((-Mathf.DeltaAngle(rotationEuler.x, 0) - (delta.y * _cameraSensitivity.y)), -80, 80);
+        newRotation.x = Mathf.Clamp((-Mathf.DeltaAngle(rotationEuler.x, 0) - (delta.y * _cameraSensitivity.y)), -_maxVerticalAngle, _maxVerticalAngle);
         newRotation.y = (rotationEuler.y + (delta.x * _cameraSensitivity.x));
         newRotation.z = 0;
 
