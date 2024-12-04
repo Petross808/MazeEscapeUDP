cbd77cc2f65fc412e294dddbe7557313446e5379
Samuel Sukovský <145551742+SamuelSukovsky@users.noreply.github.com>
2024-12-04 11:55:17 +0000

    🗝️Created Interactable, Item, Key, and playerInventory classes
    The four classes have no functionality as of now.

---
+++ Assets/MazeEscape/My Assets/Scripts/Player/PlayerInventory.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInventory : MonoBehaviour
+{
+    List<Item> _inventory = new List<Item> ();
+
+    
+}
