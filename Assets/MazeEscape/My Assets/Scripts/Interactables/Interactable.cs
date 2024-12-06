a2a93abc6e0be8e4a54ff62c3c8c1cae19aa008f
Samuel Sukovský <145551742+SamuelSukovsky@users.noreply.github.com>
2024-12-06 10:30:27 +0000

    Finished abstract interactable class
    Finished the interactable class to use the event system in order to trigger a function when interacted with

--- Assets/MazeEscape/My Assets/Scripts/Interactables/Interactable.cs
+++ Assets/MazeEscape/My Assets/Scripts/Interactables/Interactable.cs
@@ -1,11 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class Interactable : MonoBehaviour
+public abstract class Interactable : EventListenerBase
 {
-    protected void OnInteract()
+    [EventSignature(typeof (GameObject), typeof(Item))]
+    public override void OnEventRaised(GameEvent.CallbackContext context)
     {
-
+        if (context.Get<GameObject>() == this.gameObject)
+        {
+            Interact(context.Get<Item>());
+        }
     }
+
+    public abstract void Interact(Item heldItem);
 }
