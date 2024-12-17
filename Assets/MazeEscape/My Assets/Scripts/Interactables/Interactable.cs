3af2e234b3a155a6661fba689a872835fbd6230b
Petr Šulc <95595719+Petross808@users.noreply.github.com>
2024-12-17 16:32:52 +0000

    Build Fix

--- Assets/MazeEscape/My Assets/Scripts/Interactables/Interactable.cs
+++ Assets/MazeEscape/My Assets/Scripts/Interactables/Interactable.cs
@@ -1,8 +1,5 @@
 using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.EventSystems;
 
 public abstract class Interactable : EventListenerBase
 {
